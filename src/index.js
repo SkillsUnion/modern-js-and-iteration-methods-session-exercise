b3ad036c7669ba9bdc35a4e930c8e9277b9b4d1d
Billypv <88248817+Billypv@users.noreply.github.com>
2021-11-01 20:17:52 +0000

    Solved exercises

--- src/index.js
+++ src/index.js
@@ -20,28 +20,24 @@
 
 const numbers = [1, 2, 3, 4];
 let total = 0;
-for (let i = 0; i < numbers.length; i++) {
-  total += numbers[i];
-}
+numbers.forEach((number) => (total += number));
 console.log("total", total); // 10
 
 // Challenge 2: Refactor the following code using .filter()
 
 const alphabets = ["a", "b", "a", "b", "b"];
-const alphaB = [];
-alphabets.forEach((alpha) => {
-  if (alpha === "b") alphaB.push(alpha);
-});
+const alphaB = alphabets.filter((letter) => letter === "b");
 console.log("alphaB", alphaB); // [ 'b', 'b', 'b' ]
 
 // Challenge 3: Refactor the following code using .map()
 const genderShortForm = ["m", "f", "m", "f"];
-let genderSpelledForm = [];
-
-genderShortForm.forEach((x) => {
-  if (x === "m") genderSpelledForm.push("Male");
-  if (x === "f") genderSpelledForm.push("Female");
+let genderSpelledForm = genderShortForm.map((genderLetter) => {
+  if (genderLetter === "m") {
+    return "Male";
+  }
+  return "Female";
 });
+
 console.log("genderSpelledForm", genderSpelledForm);
 
 // Challenge 4: Add 'currentSweetsInJug' into the reduce function so that
@@ -51,6 +47,7 @@ const currentSweetsInJug = 4;
 const putNewSweetsInJug = [1, 2, 3];
 
 const totalSweetsInJug = putNewSweetsInJug.reduce(
-  (accumulator, currentValue) => accumulator + currentValue
+  (accumulator, currentValue) => accumulator + currentValue,
+  currentSweetsInJug
 );
 console.log("totalSweetsInJug", totalSweetsInJug); // 6
